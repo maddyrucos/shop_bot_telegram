2a11a82b067dcfaaa38e6dfdd2822437a2e32cff
igor <maddyrucos@icloud.com>
2024-09-23 21:59:21 +0300

    moved bot to another folder created api app

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+models_list = [User, Good, Categories, Product, PaymentMethod, Payment, Sale]
+
+for model in models_list:
+    admin.site.register(model)
